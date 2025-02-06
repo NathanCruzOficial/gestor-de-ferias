bc06922c4c971975967335fb3cf7a90c86ce94a2
Nathan_Cruz <nathancruzoficial@gmail.com>
2025-02-06 14:04:57 -0300

    add : gunicorn

--- run.py
+++ run.py
@@ -1,4 +1,14 @@
 from app import app
+import webview
+
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
+
+    # # Cria uma janela com um título e um site carregado
+    # webview.create_window("gestor-ferias", app)
+
+    # # Inicia a aplicação
+    # webview.start()
